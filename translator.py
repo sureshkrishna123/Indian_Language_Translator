2ebf3ec05dd9023df1918a406580b22c0945bf52
N.V.Suresh Krishna <62717096+sureshkrishna123@users.noreply.github.com>
2023-04-11 10:21:28 +0530

    Update translator.py

--- translator.py
+++ translator.py
@@ -308,6 +308,7 @@ if app_mode=='speech to text':
     st.text("Refer this link to find the language codes for src_lang and tgt_lang")
     st.markdown("https://cloud.google.com/speech-to-text/docs/speech-to-text-supported-languages")
     st.text("Example: give src_lang = en and tgt_lang = te (i.e. English to Telugu)")
+    st.text("wait for sometime to load and display the model")
     html("""<script
 	    type="module"
 	    src="https://gradio.s3-us-west-2.amazonaws.com/3.24.1/gradio.js"
